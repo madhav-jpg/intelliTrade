7bf2f1c7e039fa11a6094a199fa0d134d36ea514
madhav-jpg <madhavkotecha99@gmail.com>
2024-04-22 17:55:32 +0530

    Changed setting files

--- intelliTrade/settings.py
+++ intelliTrade/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static'),
+# ]
 
 # CELERY_BROKER_URL = ['application/json']
